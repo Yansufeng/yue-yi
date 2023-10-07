f35677423fab786675e5e26a727c51f74381c3f0
Yansufeng <392576297@qq.com>
2023-10-07 17:56:17 +0800

    fix: 修复标签样式问题

--- pages/home/components/heritage/heritage.js
+++ pages/home/components/heritage/heritage.js
@@ -11,13 +11,54 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isInit: false,
+    active: '0',
+    tabs: [],
+    list: []
+  },  
+  
+  /**
+   * 生命周期
+   */
+  lifetimes: {
+    attached: function() {
+      this.init()
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    init() {
+      this.getTabs()
+      this.getList()
+      this.setData({
+        isInit: true
+      })
+    },
 
+    getTabs() {
+      const tabs = [
+        {
+          id: '0',
+          title: '非遗项目',
+          code: '0'
+        },
+        {
+          id: '1',
+          title: '非遗传承人',
+          code: '0'
+        },
+        {
+          id: '2',
+          title: '非遗起源地',
+          code: '0'
+        }
+      ]
+      this.setData({tabs})
+    },
+  
+    getList() {}
   }
 })
