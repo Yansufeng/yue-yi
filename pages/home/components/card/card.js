d63ce12d425c15e3d1d52a5a8143e513322f3784
Yansufeng <392576297@qq.com>
2023-10-07 15:34:52 +0800

    feat: 完成样式

---
+++ pages/home/components/card/card.js
@@ -0,0 +1,33 @@
+// pages/home/components/card/card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    info: {
+      type: Object,
+      value: {
+        cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/home/bkg.png',
+        tag: '培训',
+        tagType: 0,
+        title: '春风伏笔——公益书法行动',
+        loc: '永定区文化馆',
+        date: '2023.06.21'
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
