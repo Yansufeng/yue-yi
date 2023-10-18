868caea76519296cb4a018c2e4060bb90241fdc6
Yansufeng <392576297@qq.com>
2023-10-18 17:46:36 +0800

    fix: 修复了tabbar问题

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,4 +1,6 @@
 // custom-tab-bar/index.js
+const app = getApp()
+
 Component({
   /**
    * 组件的属性列表
@@ -14,25 +16,25 @@ Component({
     active: 0,
     tabs: [{
       'id': 0,
-      'pagePath': 'pages/home/home',
+      'pagePath': '/pages/home/home',
       'text': '首页',
       'icon': 'home'
     }, 
     {
       'id': 1,
-      'pagePath': 'pages/map/map',
+      'pagePath': '/pages/map/map',
       'text': '文化地图',
       'icon': 'map-information-1'
     }, 
     {
       'id': 2,
-      'pagePath': 'pages/resource/resource',
+      'pagePath': '/pages/resource/resource',
       'text': '资源推荐',
       'icon': 'dashboard-1'
     }, 
     {
       'id': 3,
-      'pagePath': 'pages/user/user',
+      'pagePath': '/pages/user/user',
       'text': '我的',
       'icon': 'user'
     }]
@@ -58,10 +60,30 @@ Component({
         tabsL: tabs.slice(0, 2),
         tabsR: tabs.slice(2, 4)
       })
+      // this.getActive()
     },
 
-    onTap(id) {
-      console.log(id)
+    // 设置当前高亮页
+    getActive() {
+      // console.log(app.globalData.path)
+      // let path = ''
+      // let _this = this
+      // wx.onAppRoute(res => {
+      //   path = res.path
+      //   this.data.tabs.forEach(e => {
+      //     if(e.pagePath == path) _this.setData({ active: e.id })
+      //   });
+      // })
+    },
+
+    onTap(e) {
+      const id = e.currentTarget.dataset.id || 0
+      const path = this.data.tabs[id].pagePath
+      // const path = '../../' + this.data.tabs[id].pagePath
+      wx.switchTab({ url: path })
+      // this.setData({
+      //   active: id
+      // })
     }
   }
 })
