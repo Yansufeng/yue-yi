ddde32f64f1f439a3990ae42ba7e95d4de85fd55
Yansufeng <392576297@qq.com>
2023-10-20 21:49:43 +0800

    feat: 完成基本样式

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,99 @@
 // pages/user/user.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    info: {
+      name: '',
+      avatar: '',
+      check: 0
+    },
+    btns: [],
+    navs: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.getInfo()
+    this.getBtns()
+    this.getNavs()
+  },
 
+  getBtns() {
+    const btns = [
+      {
+        id: 0,
+        icon: 'order.png',
+        title: '我的预约'
+      },
+      {
+        id: 1,
+        icon: 'ticket.png',
+        title: '我的订票'
+      },
+      {
+        id: 2,
+        icon: 'point.png',
+        title: '我的积分'
+      },
+      {
+        id: 3,
+        icon: 'collect.png',
+        title: '我的收藏'
+      }
+    ]
+
+    const base = app.globalData.globalImgUrl + '/user/'
+    btns.map(item => {
+      item.icon = base + item.icon
+    })
+    
+    this.setData({btns})
+  },
+
+  getInfo() {
+    const info = {
+      name: '果果昔',
+      avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+      check: 7
+    }
+    this.setData({info})
+  },
+
+  getNavs() {
+    const navs = [
+      {
+        id: 0,
+        icon: 'user-setting',
+        title: '个人信息',
+        path: ''
+      },
+      {
+        id: 1,
+        icon: 'store',
+        title: '积分商城',
+        path: ''
+      },
+      {
+        id: 2,
+        icon: 'chat-bubble-1',
+        title: '意见反馈',
+        path: ''
+      },
+      {
+        id: 3,
+        icon: 'call-1',
+        title: '联系我们',
+        path: ''
+      },
+    ]
+    this.setData({navs})
   },
 
   /**
