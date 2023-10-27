478022da45944eb52d67e6061caf16d582b4d278
Yansufeng <392576297@qq.com>
2023-10-27 20:38:07 +0800

    feat: 新增艺术培训页面

--- pages/column/training/training.js
+++ pages/column/training/training.js
@@ -5,16 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tranningList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getList()
   },
-
+getList(){
+  const list = [
+    {
+      id: 0,
+      code: '',
+      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+      time:'2028.06.23 10:00'
+    },
+    {
+      id: 1,
+      code: '',
+      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+      time:'2023.06.23 10:00'
+    },
+    {
+      id: 2,
+      code: '',
+      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+      time:'2023.06.23 10:00'
+    },
+    {
+      id: 3,
+      code: '',
+      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+      time:'2023.06.23 10:00'
+    },
+    {
+      id: 4,
+      code: '',
+      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+      time:'2023.06.23 10:00'
+    }
+  ]
+  const now = Date.now();
+  const newlist=list.map((item)=>{
+    console.log(item)
+    item.isExpired = now > +new Date(item.time.replace(/\./g, '/'));
+    return item;
+  })
+  this.setData({tranningList:newlist})
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
