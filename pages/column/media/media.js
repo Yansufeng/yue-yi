2eabc1fdb221c3395d5481a673908918b130590a
Yansufeng <392576297@qq.com>
2023-10-28 20:52:35 +0800

    feat: 完成样式

--- pages/column/media/media.js
+++ pages/column/media/media.js
@@ -1,18 +1,154 @@
 // pages/column/media/media.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isInit: false,
+    imgs: {
+      cover: 'cover.png'
+    },
+    tabs: [],
+    teachers: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.initImgs()
+    this.getTabs()
+    this.getTeachers()
+    this.getCourses()
 
+    this.setData({
+      isInit: true
+    })
+  },
+
+  initImgs() {
+    let base = app.globalData.globalImgUrl + '/column/media/'
+
+    let imgs = this.data.imgs
+    for(let i in imgs) {
+      imgs[i] = base + imgs[i]
+    }
+    this.setData({imgs})
+  },
+
+  getTabs() {
+    const tabs = [
+      {
+        id: 0,
+        code: '',
+        name: '舞蹈'
+      },
+      {
+        id: 1,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 2,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 3,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 4,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 5,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 6,
+        code: '',
+        name: '声乐'
+      },
+      {
+        id: 7,
+        code: '',
+        name: '声乐'
+      },
+    ]
+    this.setData({tabs})
+  },
+
+  getTeachers() {
+    const teachers = [
+      {
+        id: 0,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 1,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 2,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 3,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 4,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 5,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+    ]
+    this.setData({teachers})
+  },
+
+  getCourses() {
+    const courses = [
+      {
+        id: 0,
+        cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/home/bkg.png',
+        title: '摆手舞教学（一）',
+        teacher: '张苏苏'
+      },
+      {
+        id: 1,
+        cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/home/bkg.png',
+        title: '摆手舞教学（一）',
+        teacher: '张苏苏'
+      },
+      {
+        id: 2,
+        cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/home/bkg.png',
+        title: '摆手舞教学（一）',
+        teacher: '张苏苏'
+      },
+      {
+        id: 3,
+        cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/home/bkg.png',
+        title: '摆手舞教学（一）',
+        teacher: '张苏苏'
+      },
+    ]
+    this.setData({courses})
   },
 
   /**
---
+++ pages/components/titlebar/titlebar.js
@@ -0,0 +1,35 @@
+// pages/components/titlebar/titlebar.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 标题
+    title: {
+      type: String,
+      value: ''
+    },
+    // 更多转跳链接 
+    path: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onMore() {
+      console.log(this.data.path)
+    }
+  }
+})
