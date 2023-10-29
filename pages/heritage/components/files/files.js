0eef1ecf1a3889a8f12b7786406c3c9478dab4d4
Yansufeng <392576297@qq.com>
2023-10-29 23:17:23 +0800

    feat: 完成样式

--- pages/heritage/components/files/files.js
+++ pages/heritage/components/files/files.js
@@ -12,13 +12,39 @@ Component({
    * 组件的初始数据
    */
   data: {
+    files: []
+  },
 
+  lifetimes: {
+    attached() {
+      this.getFiles()
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    getFiles() {
+      const files = [
+        {
+          id: 0,
+          cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/column/book/book.png'
+        },
+        {
+          id: 1,
+          cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/column/book/book.png'
+        },
+        {
+          id: 2,
+          cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/column/book/book.png'
+        },
+        {
+          id: 3,
+          cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/column/book/book.png'
+        }
+      ]
+      this.setData({files})
+    }
   }
 })
