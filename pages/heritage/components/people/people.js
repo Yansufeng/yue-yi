b0028f49fadf27fc619da760bae3ead800a19c94
Yansufeng <392576297@qq.com>
2023-10-29 22:47:12 +0800

    feat: 完成样式

--- pages/heritage/components/people/people.js
+++ pages/heritage/components/people/people.js
@@ -12,12 +12,14 @@ Component({
    * 组件的初始数据
    */
   data: {
-    tabs: []
+    tabs: [],
+    people: []
   },
 
   lifetimes: {
     attached() {
       this.getTabs()
+      this.getPeople()
     }
   },
 
@@ -38,5 +40,39 @@ Component({
       ]
       this.setData({ tabs })
     },
+
+    getPeople() {
+      const people = [
+        {
+          id: 0,
+          avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+          name: '张苏苏',
+          tag: '张家界阳戏',
+          des: '朱丽珍，女、土家族、1940年出生。'
+        },
+        {
+          id: 1,
+          avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+          name: '张苏苏',
+          tag: '张家界阳戏',
+          des: '朱丽珍，女、土家族、1940年出生。'
+        },
+        {
+          id: 2,
+          avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+          name: '张苏苏',
+          tag: '张家界阳戏',
+          des: '朱丽珍，女、土家族、1940年出生。'
+        },
+        {
+          id: 3,
+          avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+          name: '张苏苏',
+          tag: '张家界阳戏',
+          des: '朱丽珍，女、土家族、1940年出生。'
+        }
+      ]
+      this.setData({people})
+    }
   }
 })
