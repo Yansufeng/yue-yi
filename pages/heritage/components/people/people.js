355ab59a28f9445de566d269249f6412051abfd5
Yansufeng <392576297@qq.com>
2023-10-29 15:00:14 +0800

    feat: 增加标题

--- pages/heritage/components/people/people.js
+++ pages/heritage/components/people/people.js
@@ -12,13 +12,31 @@ Component({
    * 组件的初始数据
    */
   data: {
+    tabs: []
+  },
 
+  lifetimes: {
+    attached() {
+      this.getTabs()
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    getTabs() {
+      const tabs = [
+        {
+          id: 0,
+          name: '国家级'
+        },
+        {
+          id: 1,
+          name: '省级'
+        }
+      ]
+      this.setData({ tabs })
+    },
   }
 })
