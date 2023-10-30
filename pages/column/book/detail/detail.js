7a94d878bf10bcca8875818fca36b231ba60d555
Yansufeng <392576297@qq.com>
2023-10-31 00:22:45 +0800

    feat: 完成样式

--- pages/column/book/detail/detail.js
+++ pages/column/book/detail/detail.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    info: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.getInfo()
+  },
 
+  getInfo() {
+    const info = {
+      id: 0,
+      cover: 'https://yansufeng.github.io/img/yuanxi/yue-yi/column/book/book.png',
+      title: '无用之美',
+      des: '这是一个后真相时代，我们对于何为真实的判断暧昧晦暗。现代社会庞大的信息数据网络，除了如同迷魂阵令人难辨真假，也如同牢笼将我们封锁其中。回顾来路，20世纪的变革既漫长又短暂，一方面，科学的迅速发展建立起现代人膨胀的...'
+    }
+    this.setData({info})
   },
 
   /**
