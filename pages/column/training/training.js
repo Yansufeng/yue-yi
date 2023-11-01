1d3271139d73e429c5a4834cfcfc86c0bf1e94d2
Yansufeng <392576297@qq.com>
2023-11-01 23:41:34 +0800

    feat: 增加了转跳

--- pages/column/training/training.js
+++ pages/column/training/training.js
@@ -14,52 +14,61 @@ Page({
   onLoad(options) {
     this.getList()
   },
-getList(){
-  const list = [
-    {
-      id: 0,
-      code: '',
-      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
-      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
-      time:'2028.06.23 10:00'
-    },
-    {
-      id: 1,
-      code: '',
-      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
-      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
-      time:'2023.06.23 10:00'
-    },
-    {
-      id: 2,
-      code: '',
-      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
-      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
-      time:'2023.06.23 10:00'
-    },
-    {
-      id: 3,
-      code: '',
-      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
-      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
-      time:'2023.06.23 10:00'
-    },
-    {
-      id: 4,
-      code: '',
-      name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
-      img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
-      time:'2023.06.23 10:00'
-    }
-  ]
-  const now = Date.now();
-  const newlist=list.map((item)=>{
-    console.log(item)
-    item.isExpired = now > +new Date(item.time.replace(/\./g, '/'));
-    return item;
-  })
-  this.setData({tranningList:newlist})
-},
+
+  getList(){
+    const list = [
+      {
+        id: 0,
+        code: '',
+        name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+        img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+        time:'2028.06.23 10:00'
+      },
+      {
+        id: 1,
+        code: '',
+        name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+        img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+        time:'2023.06.23 10:00'
+      },
+      {
+        id: 2,
+        code: '',
+        name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+        img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+        time:'2023.06.23 10:00'
+      },
+      {
+        id: 3,
+        code: '',
+        name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+        img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+        time:'2023.06.23 10:00'
+      },
+      {
+        id: 4,
+        code: '',
+        name: '悦艺益课堂·公益惠民土家族民族舞蹈培训',
+        img:'https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/introduce-oteam.png',
+        time:'2023.06.23 10:00'
+      }
+    ]
+    const now = Date.now();
+    const newlist=list.map((item)=>{
+      console.log(item)
+      item.isExpired = now > +new Date(item.time.replace(/\./g, '/'));
+      return item;
+    })
+    this.setData({tranningList:newlist})
+  },
+
+  toDetail(e) {
+    const i = e.currentTarget.dataset.i
+    wx.navigateTo({
+      url: './detail/detail',
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
