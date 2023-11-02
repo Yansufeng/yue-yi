3c7f826428122cfba65b997305ed3ba7bdbb1969
Yansufeng <392576297@qq.com>
2023-11-02 22:07:33 +0800

    feat: 完成样式

--- pages/column/celebrity/celebrity.js
+++ pages/column/celebrity/celebrity.js
@@ -1,18 +1,64 @@
 // pages/column/celebrity/celebrity.js
+import { initImgs } from '../../../utils/init'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    imgs: {
+      bkg: 'main-bkg.png'
+    },
+    celebrities: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    initImgs('/column/celebrity/', this)
+    this.getCelebrities()
+  },
 
+  getCelebrities() {
+    const celebrities = [
+      {
+        id: 0,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 1,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 2,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 3,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 4,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 5,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+      {
+        id: 6,
+        avatar: 'https://yansufeng.github.io/img/yuanxi/yue-yi/user/avatar.png',
+        name: '张苏苏'
+      },
+    ]
+    this.setData({celebrities})
   },
 
   /**
