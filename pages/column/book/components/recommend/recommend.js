40c423cc35698f9ab271c955aa80e9eb172a4145
Yansufeng <392576297@qq.com>
2023-11-08 17:55:42 +0800

    feat: 对接数据

--- pages/column/book/components/recommend/recommend.js
+++ pages/column/book/components/recommend/recommend.js
@@ -1,26 +1,43 @@
 // pages/column/book/components/recommend/recommend.js
+import { getTopicResource } from '../../../../../apis/commonApi'
+
 Component({
 
   /**
    * 组件的属性列表
    */
   properties: {
-    books: {
-      type: Array
-    }
+    
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    code: 'yy-hao-shu-tui-jian'
+  },
 
+  lifetimes: {
+    attached() {
+      this.getBooks()
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    getBooks() {
+      const params = {
+        pageSize: 2
+      }
+      getTopicResource(this.data.code, params).then(res => {
+        const books = res.data.result.records
+        this.setData({books})
+        console.log(books)
+      })
+    },
+
     toDetail(e) {
       const index = e.currentTarget.dataset.i
       
