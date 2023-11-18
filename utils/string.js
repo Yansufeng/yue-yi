aa4f0e29073f93ff98c322d35180f6ebfd852660
Yansufeng <392576297@qq.com>
2023-11-18 19:13:46 +0800

    feat: 新增数据处理方法

--- utils/string.js
+++ utils/string.js
@@ -3,6 +3,22 @@ const getEllipsis = function(str, len) {
   return str.slice(0, len) + '...'
 }
 
+const getDateS = function(date) {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+
+  return `${[year, month, day].map(formatNumber).join('.')} ${[hour, minute].map(formatNumber).join(':')}`
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : `0${n}`
+}
+
 module.exports = {
-  getEllipsis
+  getEllipsis,
+  getDateS
 }
