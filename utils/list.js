4b3c3a48a8fd0f5fd893311a7762ac9ca0547993
Yansufeng <392576297@qq.com>
2023-12-09 19:48:12 +0800

    fix: 修复了分页请求bug

--- utils/list.js
+++ utils/list.js
@@ -1,16 +1,16 @@
 import { getTopicResourceData } from '../apis/commonApi'
 
 const getByPage = function(code, _this, list) {
-  const page = _this.data.page + 1 || 0
+  const page = _this.data.page + 1 || 1
   const pageSize = 5
   const params = {
     page,
     pageSize
   }
-
+  console.log(page)
   return getTopicResourceData(code, params).then(res => {
     const data = res.result.records
-    _this.setData(page)
+    _this.setData({page})
     return list.concat(...data)
   })
 }
