84fa45932dd921b0eee73b601991a51075a34beb
Yansufeng <392576297@qq.com>
2024-01-22 16:38:33 +0800

    feat: 增加了uuid生成

--- utils/string.js
+++ utils/string.js
@@ -18,7 +18,17 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+const getUuid = () => {
+  const str = []
+  const hex = '0123456789abcdefghijklmnopqrstuvwxyz'
+  for(let i = 0; i < 16; i++) {
+    str.push(hex.substr(Math.floor(Math.random() * 36), 1))
+  }
+  return str.join('')
+}
+
 module.exports = {
   getEllipsis,
-  getDateS
+  getDateS,
+  getUuid
 }
